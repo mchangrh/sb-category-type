5e966c9686011d6eddb56c50628fe6e187cea20a
Michael C <michael@mchang.name>
2022-02-16 18:18:32 -0500

    bump for EMOJI_ID_MAP

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ const CATEGORY_TYPES_MAP = {
   "filler": ["skip", "mute"],
   "exclusive_access": ["full"],
 }
+const EMOJI_ID_MAP = {
+  sponsor: { name: "sponsor", id: "936878146156892240" },
+  intro: { name: "intro", id: "936878146391769108" },
+  outro: { name: "outro", id: "936878146135920700" },
+  interaction_reminder: { name: "interaction_reminder", id: "936878145993322557" },
+  selfpromo: { name: "selfpromo:", id: "936878146228207636" },
+  nonmusic: { name: "nonmusic", id: "936878146186252288" },
+  preview: { name: "preview", id: "936878146190471178" },
+  highlight: { name: "highlight", id: "936878146316292106" },
+  filler: { name: "filler", id: "936878145812971581" },
+  exclusive_access: { name: "exclusive_access", id: "936878145909424179" }
+};
 
 //helper
 const createMap = (keys, values) =>
@@ -21,8 +33,7 @@ const createMap = (keys, values) =>
 const CATEGORY_NAMES = Object.keys(CATEGORY_TYPES_MAP)
 const TYPES = [...new Set(Object.values(CATEGORY_TYPES_MAP).flat())]
 const COLOUR_ARR = [0x00D400, 0x00FFFF, 0x0202ED, 0xCC00FF, 0xFFFF00, 0xFF9900, 0x008FD6, 0xFF1684, 0x7300FF, 0x008A5C];
-const EMOJI_ARR = ["<:sponsor:936878146156892240>", "<:intro:936878146391769108>","<:outro:936878146135920700>","<:interaction_reminder:936878145993322557>","<:selfpromo:936878146228207636>","<:nonmusic:936878146186252288>", "<:preview:936878146190471178>","<:highlight:936878146316292106>","<:filler:936878145812971581>","<:exclusive_access:936878145909424179>"];
-
+const EMOJI_ARR = Object.values(EMOJI_ID_MAP).map((o) => `<:${o.name}:${o.id}>`);
 const EMOJI_MAP = createMap(CATEGORY_NAMES, EMOJI_ARR);
 const COLOUR_MAP = createMap(CATEGORY_NAMES, COLOUR_ARR);
 
@@ -34,6 +45,7 @@ module.exports = {
   CATEGORY_TYPES_MAP,
   CATEGORY_NAMES,
   TYPES,
+  EMOJI_ID_MAP,
   EMOJI_MAP,
   COLOUR_MAP,
   valiateCategoryType
